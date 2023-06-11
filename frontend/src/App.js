3c432644595ebabf3d059f55ac3271e5d9d14ae2
Nitish.mishra1 <Nitish.mishra1@RBLLNHO0424>
2023-06-11 19:11:52 +0530

    commit for home page and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,17 @@
-import React from "react";
-import { ReactNavbar } from "overlay-navbar";
-import logo from "./images/logo.png";
-
-// const options = {
+import './App.css';
+import Header from './component/layout/header/Header.js'
+import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
+import Footer from './component/layout/footer/Footer.js'
+import Home from './component/home/Home.js'
+function App() {
+  return <Router>
+     <Header></Header>
+     <Routes>
+     <Route exact path='/' Component={Home}></Route>
+     </Routes>
+     <Footer></Footer>
+  </Router>
  
-// };
+}
 
-const Header = () => {
-  return <ReactNavbar
-  burgerColorHover= "#eb4034"
-  logo={logo}
-  logoWidth= "20vmax"
-  navColor1= "white"
-  logoHoverSize= "10px"
-  logoHoverColor= "#eb4034"
-  link1Text= "Home"
-  link2Text= "Products"
-  link3Text= "Contact"
-  link4Text= "About"
-  link1Url= "/"
-  link2Url= "/products"
-  link3Url= "/contact"
-  link4Url= "/about"
-  link1Size= "1.3vmax"
-  link1Colo= "rgba(35, 35, 35,0.8)"
-  nav1justifyContent= "flex-end"
-  nav2justifyContent= "flex-end"
-  nav3justifyContent= "flex-start"
-  nav4justifyContent= "flex-start"
-  link1ColorHover= "#eb4034"
-  link1Margin= "1vmax"
-  profileIconUrl= "/login"
-  profileIconColor= "rgba(35, 35, 35,0.8)"
-  searchIconColor= "rgba(35, 35, 35,0.8)"
-  cartIconColor= "rgba(35, 35, 35,0.8)"
-  profileIconColorHover= "#eb4034"
-  searchIconColorHover= "#eb4034"
-  cartIconColorHover= "#eb4034"
-  cartIconMargin= "1vmax"
-   />;
-};
-
-export default Header;
+export default App;
---
+++ frontend/src/component/home/Home.js
@@ -0,0 +1,69 @@
+import React, { Fragment } from "react";
+//import { CgMouse } from "react-icons/all";
+import "./Home.css";
+import Product from "./Product.js";
+//import MetaData from "../layout/MetaData";
+// import { clearErrors, getProduct } from "../../actions/productAction";
+// import { useSelector, useDispatch } from "react-redux";
+// import Loader from "../layout/Loader/Loader";
+// import { useAlert } from "react-alert";
+
+const Home = () => {
+//   const alert = useAlert();
+//   const dispatch = useDispatch();
+//   const { loading, error, products } = useSelector((state) => state.products);
+
+//   useEffect(() => {
+//     if (error) {
+//       alert.error(error);
+//       dispatch(clearErrors());
+//     }
+//     dispatch(getProduct());
+//   }, [dispatch, error, alert]);
+const product={
+    name:'Mobile',
+    images:[{url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxEC5BqiDdX_xw-GoaY-CUHLTCmU1zX2lBhXhnRp-R&s'}],
+    price:'2000',
+    _id:'mishra',
+}
+  return (
+    <Fragment>
+      {/* {loading ? (
+        <Loader />
+      ) : ( */}
+        <Fragment>
+          {/* <MetaData title="ECOMMERCE" /> */}
+
+          <div className="banner">
+            <p>Welcome to Ecommerce</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
+            <a href="#container">
+              <button>
+                Scroll 
+              </button>
+            </a>
+          </div>
+
+          <h2 className="homeHeading">Featured Products</h2>
+
+          <div className="container" id="container">
+            {/* {products &&
+              products.map((product) => ( */}
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+              {/* ))} */}
+          </div>
+        </Fragment>
+      {/* )} */}
+    </Fragment>
+  );
+};
+
+export default Home;
